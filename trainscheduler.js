5549f4162764fc8659c081c7d608191debf670aa
Danilo Kim-Triana <kimhodori@gmail.com>
2017-11-17 16:56:09 -0800

    Add files via upload
    Add moment.js, fixed .js syntax

--- trainscheduler.js
+++ trainscheduler.js
@@ -1,4 +1,4 @@
-// Firebase Link
+
 //initialize firebase
 
   var config = {
@@ -19,26 +19,26 @@ $("#add-train-btn").on("click",function(event){
 	// Grabs train input
 	var traName = $("#train-name-input").val().trim();
 	var traDestination = $("#destination-input").val().trim();
-	var traFirstTime = $("#first-input").val().trim();
+	var priTraName = moment($("#first-TraName-input").val().trim(), "DD/MM/YY").format("X");
 	var traFrecuency = $("#frecuency-input").val().trim();
 
 	// Local "temporary" object holding emp.data
 
 	var newTrain = {
-		name: traName,
+		trainName: traName,
 		destination: traDestination,
-		start: traFirstTime,
+		firstTrainName: priTraName,
 		frecuency: traFrecuency
 	};
 
 	// Uploads train data to the database
 	database.ref().push(newTrain);
 
-	// Logs everything to console
-	console.log(traName.name);
-	console.log(traName.destination);
-	console.log(traName.start);
-	console.log(traName.frecuency);
+	// // Logs everything to console
+	// console.log(newTrain.trainName);
+	// console.log(newTrain.destination);
+	// console.log(newTrain.firstTrainName);
+	// console.log(newTrain.frecuency);
 
 	// Alert
 	alert("Train succesfully added");
@@ -46,27 +46,44 @@ $("#add-train-btn").on("click",function(event){
 	// Clears all of the text-boxes
 	$("#train-name-input").val("");
 	$("#destination-input").val("");
-	$("#first-input").val("");
+	$("#firstTraName-input").val("");
 	$("#frecuency-input").val("");
+	});
 
-});
-
-// Creates firebase event for adding train to the database and when the user adss an entry
-database.ref().on("child_added", function(childSnapShot, prevChildKey) {
+	// Creates firebase event for adding train to the database and when the user adss an entry
+	database.ref().on("child_added", function(childSnapShot, prevChildKey) {
 
 	console.log(childSnapShot.val());
 
-	// store everything into 1 var.
-	var traName = childSnapShot.val().name;
+	// store everything into a var.
+	var traName = childSnapShot.val().trainName;
 	var traDestination = childSnapShot.val().destination;
-	var traFirstTime = childSnapShot.val().start;
+	var priTraName = childSnapShot.val().firstTrainName;
 	var traFrecuency = childSnapShot.val().frecuency;
 
 	//  Train info
-	console.log(traName);
-	console.log(traDestination);
-	console.log(traFirstTime);
-	console.log(traFrecuency);
-})
+	// console.log(traName);
+	// console.log(traDestination);
+	// console.log(priTraName);
+	// console.log(traFrecuency);
+
+	var differenceTimes = moment().diff(moment.unix(priTraName), "minutes");
+	var remainder = moment().dif(momment.unix(priTraName), "minutes") % traFrecuency;
+	var minutes = traFrecuency - remainder;
+
+	var arrival =  moment().add(minutes, "m").format("hh:mm A");
+	console.log(minutes);
+	console.log(arrival);
+	console.log(moment().format("hh:mm A"));
+    console.log(arrival);
+    console.log(moment().format("X"));
+
+    // Append train data to table 
+    $("#trainSchedule > tbody").append("<tr><td>" + traName + "</td><td>" + traDestination + "</td><td>" + priTraName + "</td><td>" + arrival + "</td><td>" + minutes + "</td></tr>");
+});
 
 
+
+	
+
+	
