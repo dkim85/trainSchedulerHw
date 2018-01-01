52f6c91b74ed8f934e71ee38a16894d9ab92f5a2
Danilo Kim <danilo@danilos-macbook.attlocal.net>
2018-01-01 01:09:45 -0800

    functional app syntax fixed

--- trainscheduler.js
+++ trainscheduler.js
@@ -5,10 +5,10 @@
   // Initiate Firebase
 
   var config = {
-    apiKey: "AIzaSyA6N0uRiiMMDtYgpr-l4qhXDvGh-bDsicU",
-    authDomain: "test-2104e.firebaseapp.com",
-    databaseURL: "https://test-2104e.firebaseio.com",
-    storageBucket: "test-2104e.appspot.com",
+    apiKey: "AIzaSyAsC8xQZAAZqNZvRT4sRD7YPI_Nssj8n48",
+    authDomain: "trainhw-2fad0.firebaseapp.com",
+    databaseURL: "https://trainhw-2fad0.firebaseio.com",
+    storageBucket: "",
   };
   
 firebase.initializeApp(config);
@@ -17,22 +17,23 @@ var trainData = firebase.database();
 
 
 // Grabbin train input
-$("#addTrainBtn").on("click",function(){
-	var trainName = $("#trainNameInput").val().trim();
+$("#addTrainBtn").on("click", function(){
+
+	var tName = $("#trainNameInput").val().trim();
 	var destination = $("#destinationInput").val().trim();
 	var firstTrain = moment($("#firstTrainInput").val().trim(),"HH:mm").subtract(10,"years").format("X");
 	var frequency = $("#frequencyInput").val().trim();
 
 	// console.log(firstTrain);
 	// return false;
-	var newTrain = {
-		name: trainName,
+	firebase.database().ref().push( {
+		tName: tName,
 		destination: destination,
 		firstTrain: firstTrain,
 		frequency: frequency
-	};
+	});
 
-	trainData.ref().push(newTrain);
+	// trainData.ref().push(newTrain);
 
 	// Logs everything to console
 	  // console.log(newTrain.name);
@@ -51,7 +52,7 @@ $("#addTrainBtn").on("click",function(){
 
 });
 
-trainData.ref().on("child_added",function(snapshot){
+trainData.ref().on("child_added",function(snapshot, preChildKey){
 	var name = snapshot.val().name;
 	var destination = snapshot.val().destination;
 	var frequency = snapshot.val().frequency;
